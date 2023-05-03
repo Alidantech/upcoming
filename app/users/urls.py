8efa8585bbf51e095ddd627092f3f0336d70b079
PETER IRUNGU <peteralidante254@gmail.com>
2023-05-03 17:44:53 +0300

    about page

--- app/users/urls.py
+++ app/users/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('account/', views.account, name='account'),
+    path('about/', views.about, name='about'),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -8,3 +8,7 @@ def account(request):
 def main(request):
   template = loader.get_template('main.html')
   return HttpResponse(template.render())
+
+def about(request):
+  template = loader.get_template('about.html')
+  return HttpResponse(template.render())
