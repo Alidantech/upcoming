68348daf0ef018955159cc2352733be19f36554b
PETER IRUNGU <peteralidante254@gmail.com>
2023-05-07 16:55:17 +0300

    designing music page

--- app/music/static/js/music.js
+++ app/music/static/js/music.js
@@ -1,4 +1,3 @@
-
 const content = document.querySelector('.music-container');
 const scrollRightButton = document.querySelector('.scroll-btn.right');
 const scrollLeftButton = document.querySelector('.scroll-btn.left');
@@ -8,6 +7,7 @@ scrollRightButton.addEventListener('click', () => {
     left: 100, // set your desired scroll distance
     behavior: 'smooth' // enable smooth scrolling
   });
+  checkScrollPosition();
 });
 
 scrollLeftButton.addEventListener('click', () => {
@@ -15,4 +15,21 @@ scrollLeftButton.addEventListener('click', () => {
     left: -100, // set your desired scroll distance
     behavior: 'smooth' // enable smooth scrolling
   });
+  checkScrollPosition();
 });
+
+function checkScrollPosition() {
+  // hide left button if already at the beginning
+  if (content.scrollLeft === 0) {
+    scrollLeftButton.style.display = 'none';
+  } else {
+    scrollLeftButton.style.display = 'block';
+  }
+
+  // hide right button if already at the end
+  if (content.scrollWidth - content.scrollLeft === content.clientWidth) {
+    scrollRightButton.style.display = 'none';
+  } else {
+    scrollRightButton.style.display = 'block';
+  }
+}
