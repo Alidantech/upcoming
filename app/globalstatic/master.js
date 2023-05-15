86db7470a3b2a3d512c92c02ab1e14e85d04ee28
PETER IRUNGU <peteralidante254@gmail.com>
2023-05-15 17:46:58 +0300

    music updates

--- app/globalstatic/master.js
+++ app/globalstatic/master.js
@@ -1,11 +1,12 @@
-function showNavigation(){
-   var navigator = document.getElementById('navigator');
-   if(navigator.style.display === 'none'){
-    navigator.style.display = 'block';
-    navigator.style.height = "90%";
-   }else {
-    navigator.style.display = 'none';
+const buttons = document.querySelectorAll('button[title]');
 
-   }
-   console.log("clicked")
-}
+buttons.forEach(button => {
+  button.addEventListener('mouseenter', () => {
+    button.removeAttribute('title');
+  });
+
+  button.addEventListener('mouseleave', () => {
+    const tooltip = button.getAttribute('data-tooltip');
+    button.setAttribute('title', tooltip);
+  });
+});
