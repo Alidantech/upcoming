abed690ac18d2475b2948ea2dbdf4bfe7f52723c
PETER IRUNGU <peteralidante254@gmail.com>
2023-06-03 15:24:48 +0300

    register views and template

--- src/users/urls.py
+++ src/users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from .views import register
+
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('account/', views.account, name='account'),
     path('about/', views.about, name='about'),
-    path('artists/', views.artists, name = 'artists')
+    path('artists/', views.artists, name = 'artists'),
+    path('register/', register, name='register'),
 ]
--- src/users/views.py
+++ src/users/views.py
@@ -1,5 +1,20 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.shortcuts import render, redirect
+
 from django.http import HttpResponse
 from django.template import loader
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')  # Replace 'home' with the URL name for your home page
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
 def account(request):
   template = loader.get_template('register.html')
