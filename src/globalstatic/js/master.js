85deae15e9cab3ec574b4f89431378188ee1961f
PETER IRUNGU <peteralidante254@gmail.com>
2023-06-14 17:50:06 +0300

    creating user theme pref

--- src/globalstatic/js/master.js
+++ src/globalstatic/js/master.js
@@ -1,3 +1,6 @@
+ /*
+*Adds a class of header scrolled when a user starts scrolling
+*/ 
   document.addEventListener('DOMContentLoaded', function() {
   window.addEventListener('scroll', function() {
     if (window.pageYOffset > 0) {
@@ -7,3 +10,40 @@
     }
   });
 });
+
+// Check user's theme on page load
+(function() {
+  const prefersDarkTheme = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const savedTheme = localStorage.getItem('theme');
+  const root = document.documentElement;
+  const initialTheme = savedTheme || (prefersDarkTheme ? 'dark' : 'light');
+  root.setAttribute('data-theme', initialTheme);
+  const rootCSSFile = initialTheme === 'dark' ? 'root-dark.css' : 'root-light.css';
+  loadCSS(rootCSSFile);
+})();
+
+// Changes the theme of the page when a user clicks the theme toggle button
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+  const root = document.documentElement;
+  const currentTheme = root.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+  root.setAttribute('data-theme', newTheme);
+
+  // Save the theme preference in localStorage
+  localStorage.setItem('theme', newTheme);
+
+  // Load the corresponding root CSS file based on the selected theme
+  const rootCSSFile = newTheme === 'dark' ? 'root-dark.css' : 'root-light.css';
+  loadCSS(rootCSSFile);
+});
+
+// Function to dynamically load a CSS file
+function loadCSS(filename) {
+  const link = document.createElement('link');
+  link.href = `/static/css/${filename}`;  // Modify the path to the CSS file
+  link.rel = 'stylesheet';
+  link.type = 'text/css';
+  document.head.appendChild(link);
+}
